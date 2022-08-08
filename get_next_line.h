14d724156ee6e1752d06a1e13c029ff29c1467c1
darioroblesss <drobles@student.42madrid.com>
2022-08-08 13:45:12 +0200

    inicio

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/08 13:36:13 by marvin            #+#    #+#             */
+/*   Updated: 2022/08/08 13:36:13 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE.H
+#   define GET_NEXT_LINE.H
+
+char *get_next_line(int fd);
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include 
+
